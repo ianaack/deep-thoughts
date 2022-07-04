df7fefc5775970c6b4271bb9abca547910f91d82
ianaack <ianaack@gmail.com>
2022-07-04 13:59:14 -0400

    Full Stack Server Structure

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -20,6 +21,15 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
 	await server.start();
 	server.applyMiddleware({ app });
+
+	// Serve up static assets
+	if (process.env.NODE_ENV === "production") {
+		app.use(express.static(path.join(__dirname, "../client/build")));
+	}
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../client/build/index.html"));
+	});
 
 	db.once("open", () => {
 		app.listen(PORT, () => {
